752308e7e9bc82e7ff9f4be374337aa996e32d50
Johnathan Law <johnathanlaw@gmail.com>
2015-03-22 17:35:24 +0000

    Begins work on login logic and currency

--- ismo-client/src/co/ismo/gui/Main.java
+++ ismo-client/src/co/ismo/gui/Main.java
@@ -1,21 +1,59 @@
 package co.ismo.gui;
 
+import co.ismo.util.Constants;
+
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    private Group group;
+
+    private void setupDisplay(Stage stage) {
+        group = new Group();
+        Scene scene = new Scene(group);
+        scene.getStylesheets().add("/co/ismo/res/css/main.css");
+        stage.setScene(scene);
+
+        Rectangle2D primaryDisplay = Screen.getPrimary().getBounds();
+        Rectangle r = new Rectangle(primaryDisplay.getWidth(), primaryDisplay.getHeight());
+        r.getStyleClass().add("rect");
+        group.getChildren().add(r);
+
+        stage.setX(primaryDisplay.getMinX());
+        stage.setY(primaryDisplay.getMinY());
+        stage.setHeight(primaryDisplay.getHeight());
+        stage.setWidth(primaryDisplay.getWidth());
+
+        stage.setFullScreen(true);
+        stage.setAlwaysOnTop(true);
+        stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+        stage.setFullScreenExitHint("");
+
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                if (!Constants.DEVELOPER_MODE) { event.consume(); }
+                System.out.println("Window Close event consumed");
+            }
+        });
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        setupDisplay(stage);
+        stage.show();
+    }
 
     public static void main(String[] args) {
         launch(args);
---
+++ ismo-client/src/co/ismo/util/Constants.java
@@ -0,0 +1,5 @@
+package co.ismo.util;
+
+public abstract class Constants {
+    public static final boolean DEVELOPER_MODE = true;
+}
